973c234eec321dbc722bb26ef126e1b2449637b3
Sakhmina <asadullaevasakhmina@gmail.com>
2022-09-12 20:02:48 +0300

    Implement the methods of PostRepository

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,25 +1,46 @@
 package ru.netology.repository;
 
+import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 // Stub
 public class PostRepository {
-  public List<Post> all() {
-    return Collections.emptyList();
-  }
 
-  public Optional<Post> getById(long id) {
-    return Optional.empty();
-  }
+    private final ConcurrentHashMap<Long, Post> postsRepository = new ConcurrentHashMap<>();
+    static Long postId = Long.valueOf(0);
 
-  public Post save(Post post) {
-    return post;
-  }
+    public List<Post> all() {
+        if (!postsRepository.isEmpty()) return new ArrayList<>(postsRepository.values());
+        else throw new NullPointerException("the repository is empty");
+    }
 
-  public void removeById(long id) {
-  }
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(postsRepository.get(id));
+    }
+
+    public Post save(Post post) throws NotFoundException {
+
+        if (post.getId() == 0) {
+            //create new post
+            post.setId(postId);
+            postId++;
+        } else if (postsRepository.containsKey(post.getId())) {
+            //update id number of the post
+            postsRepository.put(post.getId(), post);
+        } else throw new NotFoundException("Post with that id doesn't exist");
+        return post;
+    }
+
+    public void removeById(long id) throws NotFoundException {
+        if (postsRepository.containsKey(id)) {
+            postsRepository.remove(id);
+        } else {
+            throw new NotFoundException("Post with that id doesn't exist");
+        }
+    }
 }
